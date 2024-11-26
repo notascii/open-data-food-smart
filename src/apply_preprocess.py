e964a5a4e2d455cc9a45f562171a0894e9205215
notascii <kearzzcodeur@gmail.com>
2024-11-26 16:22:31 +0100

    apply-preprocess: apply given current countries acronyms

--- src/apply_preprocess.py
+++ src/apply_preprocess.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from countries import country_acronyms
+from countries import country_acronyms as acronyms
 
 DATA_DIR_RELATIVE_PATH = "../data/"
-
 DATASET_PATH = DATA_DIR_RELATIVE_PATH + "en.openfoodfacts.org.products.csv"
 
 def main():
@@ -18,20 +17,44 @@ def main():
         usecols=COLUMNS,
         sep="\t",
         on_bad_lines='skip',
-        nrows=500,
     ).dropna(subset=COLUMNS)  # Drop rows with any NaN
 
-    # split origins tags in a list, and remove prefix (if any) for each tag in the list
-    df["origins_tags_split"] = df["origins_tags"].apply(
-        lambda tags: [ tag.strip()[3:] if ":" in tag else tag.strip() for tag in tags.split(',') ]
+    columns_to_work = ["origins_tags", "manufacturing_places_tags", "countries_tags"]
+    for col in columns_to_work:
+        # split origins tags in a list, and remove prefix (if any) for each tag in the list
+        df[f"{col}_std"] = df[col].apply(
+            lambda tags: [
+                tag.strip()[3:].replace("-", " ")
+                if ":" in tag
+                else tag.strip().replace("-", " ")
+                for tag in tags.split(',')
+            ]
+        )
+
+        # replace every alternative names with it's standard name as defined in country_acronyms
+        df[f"{col}_std"] = df[f"{col}_std"].apply(
+            lambda tags: [ acronyms.get_group(tag, f"NF:{tag}") for tag in tags ]
+        )
+
+    # filter out rows containing "NF:"
+    # Create a mask that checks ALL specified columns
+    columns_filtered = [col + "_std" for col in columns_to_work]
+    mask = ~df[columns_filtered].apply(
+        lambda row: any(
+            any(str(val).startswith('NF:') for val in col_list) 
+            for col_list in row
+        ), 
+        axis=1
+    )
+    filtered_df = df[mask]
+
+    filtered_df.to_csv(
+        DATA_DIR_RELATIVE_PATH + "cleaned_data_v2.csv",
+        sep=",",
+        columns=["product_name", "origins_tags_std", "manufacturing_places_tags_std", "countries_tags_std"],
+        index=False,
     )
 
-    # replace every alternative names with it's standard name as defined in country_acronyms
-    df["origins_tags_split_std"] = df["origins_tags_split"].apply(
-        lambda tags: [ country_acronyms.get_group(tag, f"NF:{tag}") for tag in tags ]
-    )
-
-    print(df)
 
 if __name__ == "__main__":
     main()
