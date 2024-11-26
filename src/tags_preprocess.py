f106e7122a3e6fff57fbaeb5383f26bf0bc6642e
notascii <kearzzcodeur@gmail.com>
2024-11-26 13:38:47 +0100

    preprocess: remove '-' and replace them with a space in origins tags

--- src/tags_preprocess.py
+++ src/tags_preprocess.py
@@ -19,14 +19,28 @@ df = pd.read_csv(
     usecols=COLUMNS,
     sep="\t",
     on_bad_lines='skip',
+    nrows=100,
 ).dropna(subset=COLUMNS)  # Drop rows with any NaN
 
-df["tags_split"] = df["countries_tags"].apply(lambda tags: [tag.strip()[3:] if ":" in tag else tag.strip() for tag in tags.split(',')])
+# split tags string by comma into a list and
+# remove language prefixes (if any) (e.g. 'en', 'fr') for each tag
+# and replace every '-' with ' '
+df["tags_split"] = df["countries_tags"].apply(
+    lambda tags: [
+        tag.strip()[3:].replace("-", " ")
+        if ":" in tag
+        else tag.strip().replace("-", " ")
+        for tag in tags.split(',')
+    ]
+)
 
+# explode dataframe around the list of tags
 df_explode_origins = df.explode("tags_split")
 
+# new dataframe contains only rows for which origin tag have not been translated by 'country_acronyms' yet
 df_unlisted = df_explode_origins[~df_explode_origins["tags_split"].isin(country_acronyms.get_all_elts())]
 
+# count occurrences of each unlisted tag
 df_value = df_unlisted.value_counts("tags_split", dropna=False)
 
 for country, count in df_value.items():
