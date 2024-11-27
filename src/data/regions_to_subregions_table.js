28fc18aecc4a9e7a6d45613e5537e3280ba9d26a
GrathwohlHugo <hugo.grathwohl@grenoble-inp.org>
2024-11-27 16:05:15 +0100

    make country to subregion to region and vice versa accessible in js

---
+++ src/data/regions_to_subregions_table.js
@@ -0,0 +1,34 @@
+const regions_to_subregions = {
+    "Africa": [
+        "Africa",
+        "Sub-Saharan Africa",
+        "Northern Africa"
+    ],
+    "Americas": [
+        "Americas",
+        "Latin America and the Caribbean",
+        "Northern America"
+    ],
+    "Oceania": [
+        "Polynesia",
+        "Melanesia",
+        "Australia and New Zealand",
+        "Oceania",
+        "Micronesia"
+    ],
+    "Asia": [
+        "Central Asia",
+        "Eastern Asia",
+        "Western Asia",
+        "Southern Asia",
+        "Asia",
+        "South-eastern Asia"
+    ],
+    "Europe": [
+        "Western Europe",
+        "Northern Europe",
+        "Southern Europe",
+        "Eastern Europe",
+        "Europe"
+    ]
+};
