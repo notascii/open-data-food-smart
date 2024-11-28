7062b450f752da23dcf7fdc15830004a7f06b297
notascii <kearzzcodeur@gmail.com>
2024-11-28 14:02:24 +0100

    data: mapping groups 1->2 and 2->1

---
+++ src/data/g1_to_g2.js
@@ -0,0 +1,62 @@
+const g1_to_g2 = {
+    "Alcoholic beverages": [
+        "Alcoholic beverages"
+    ],
+    "Beverages": [
+        "Artificially sweetened beverages",
+        "Fruit juices",
+        "Fruit nectars",
+        "Unsweetened beverages",
+        "Sweetened beverages",
+        "Teas and herbal teas and coffees",
+        "Waters and flavored waters",
+        "Plant-based milk substitutes"
+    ],
+    "Cereals and potatoes": [
+        "Cereals",
+        "Bread",
+        "Legumes",
+        "Breakfast cereals",
+        "Potatoes"
+    ],
+    "Composite foods": [
+        "Sandwiches",
+        "One-dish meals",
+        "Pizza pies and quiches",
+        "Sandwich"
+    ],
+    "Fat and sauces": [
+        "Fats",
+        "Dressings and sauces"
+    ],
+    "Fish Meat Eggs": [
+        "Meat",
+        "Fish and seafood",
+        "Processed meat",
+        "Offals",
+        "Eggs"
+    ],
+    "Fruits and vegetables": [
+        "Vegetables",
+        "Dried fruits",
+        "Fruits",
+        "Soups"
+    ],
+    "Milk and dairy products": [
+        "Cheese",
+        "Dairy desserts",
+        "Milk and yogurt",
+        "Ice cream"
+    ],
+    "Salty snacks": [
+        "Nuts",
+        "Appetizers",
+        "Salty and fatty products"
+    ],
+    "Sugary snacks": [
+        "Chocolate products",
+        "Biscuits and cakes",
+        "Sweets",
+        "Pastries"
+    ]
+}
